60cf1eca79ffed0071cf770e32275e30903c2646
blvckcoffee <joe.johnson.dev@gmail.com>
2021-01-26 20:39:09 -0600

    Initial commit. Built a small javascript number guessing game

---
+++ app.js
@@ -0,0 +1,49 @@
+// Here's some variable initialization
+
+let randomNumber = Math.floor(Math.random() * 100) + 1
+let guessesRemaining = 10
+
+// Let's make some selections in the browser
+
+const game = document.querySelector('#game'),
+  guessBtn = document.querySelector('.guess-btn'),
+  guessInput = document.querySelector('#guessInput'),
+  message = document.querySelector('.message')
+
+// Listen for a user's guess on a mouse click
+
+guessBtn.addEventListener('click', function () {
+  let guess = parseInt(guessInput.value)
+
+  // win condition
+
+  if (guess === randomNumber) {
+    setMessage(
+      `Congratulations. ${randomNumber} is correct. You should be really proud of yourself.`
+    )
+  } else {
+    // incorrect guess
+    guessesRemaining -= 1
+    // lose condition
+    if (guessesRemaining === 0) {
+      setMessage(
+        `You aren't very good at this. The correct number was ${randomNumber}.`
+      )
+    } else if (guess > randomNumber) {
+      guessInput.value = ''
+      setMessage(
+        `${guess} is too high, you have ${guessesRemaining} guesses left.`
+      )
+    } else {
+      setMessage(
+        `${guess} is too low, you have ${guessesRemaining} guesses left.`
+      )
+    }
+  }
+})
+
+// set message to print below guess text field
+
+function setMessage(msg) {
+  message.textContent = msg
+}
