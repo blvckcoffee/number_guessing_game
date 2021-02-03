eca678a0edeae0473affb1b9f39fa64a1c75c446
blvckcoffee <joe.johnson.dev@gmail.com>
2021-02-03 14:57:07 -0600

    added additional functionality per latest assignment doc

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
 // Here's some variable initialization
 
-let randomNumber = Math.floor(Math.random() * 100) + 1
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let guessesRemaining = 10;
+let totalGuesses = 0;
 
 // Let's make some selections in the browser
 
 const game = document.querySelector('#game'),
   guessBtn = document.querySelector('.guess-btn'),
   guessInput = document.querySelector('#guessInput'),
-  message = document.querySelector('.message')
+  message = document.querySelector('.message');
 
 // Listen for a user's guess on a mouse click
 
 guessBtn.addEventListener('click', function () {
-  let guess = Number(guessInput.value).toFixed(2)
+  let guess = parseInt(guessInput.value);
   // check for stupidity
   if (guess > 100 || guess < 1 || isNaN(guess)) {
-    setMessage(`I asked for a number between 1 and 100.`)
+    setMessage(`I asked for an INTEGER between 1 and 100.`);
   }
   // win condition
   else if (guess === randomNumber) {
     setMessage(
-      `Congratulations. ${randomNumber} is correct. You should be really proud of yourself.`
-    )
+      `Congratulations. ${randomNumber} is correct. It took you ${totalGuesses} total guesses. You should be really proud of yourself.`
+    );
   } else {
-    if (guess > randomNumber) {
-      guessInput.value = ''
-      setMessage(`${guess} is too high.`)
+    // incorrect guess
+    guessesRemaining -= 1;
+    totalGuesses += 1;
+    // lose condition
+    if (guessesRemaining === 0) {
+      setMessage(
+        `You aren't very good at this. The correct number was ${randomNumber}.`
+      );
+    } else if (guess > randomNumber) {
+      guessInput.value = '';
+      setMessage(
+        `${guess} is too high. You have taken ${totalGuesses} guesses. You have ${guessesRemaining} guesses left.`
+      );
     } else {
-      setMessage(`${guess} is too low.`)
+      setMessage(
+        `${guess} is too low. You have taken ${totalGuesses} guesses. You have ${guessesRemaining} guesses left.`
+      );
     }
   }
-})
+});
 
 // set message to print below guess text field
 
 function setMessage(msg) {
-  message.textContent = msg
+  message.textContent = msg;
 }
